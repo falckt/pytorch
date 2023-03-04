5fa1f651a22343a8192ea1526617053c9fa29496
Zachary DeVito <zdevito@gmail.com>
2023-03-04 01:08:46 +0000

    [memory debugging] Extract frame information from inductor
    ghstack-source-id: 07daa8b19c66bfdffe8d087d68e81951bd0b9ba8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95753

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -11,7 +11,7 @@ import tempfile
 import textwrap
 import time
 from io import StringIO
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, NamedTuple, Optional, Union
 from unittest import mock
 
 import sympy
@@ -364,6 +364,10 @@ def get_dtype_size(dtype):
     return torch.empty((), dtype=dtype).element_size()
 
 
+class LineContext(NamedTuple):
+    context: Any
+
+
 class IndentedBuffer:
     tabwidth = 4
 
@@ -371,13 +375,21 @@ class IndentedBuffer:
         self._lines = []
         self._indent = initial_indent
 
-    def getvalue(self, max_lines=None):
+    def getvaluewithlinemap(
+        self,
+        max_lines=None
+    ):
         buf = StringIO()
-        for lineno, line in enumerate(self._lines):
+        p = 1
+        linemap = []
+        for line in self._lines:
             if isinstance(line, DeferredLineBase):
                 line = line()
                 if line is None:
                     continue
+            elif isinstance(line, LineContext):
+                linemap.append((p, line.context))
+                continue
             assert isinstance(line, str)
             buf.write(line)
             buf.write("\n")
@@ -388,7 +400,13 @@ class IndentedBuffer:
                 )
                 break
 
-        return buf.getvalue()
+            # note: some 'line's contain '\n'
+            p += 1 + line.count("\n")
+        return buf.getvalue(), linemap
+
+    def getvalue(self, max_lines=None):
+        v, _ = self.getvaluewithlinemap(max_lines)
+        return v
 
     def getrawvalue(self):
         buf = StringIO()
@@ -397,6 +415,8 @@ class IndentedBuffer:
                 line = line()
                 if line is None:
                     continue
+            elif isinstance(line, LineContext):
+                continue
             assert isinstance(line, str)
             # backslash implies line continuation
             if line.endswith("\\"):
@@ -416,7 +436,9 @@ class IndentedBuffer:
         return " " * (self._indent * self.tabwidth)
 
     def writeline(self, line):
-        if isinstance(line, DeferredLineBase):
+        if isinstance(line, LineContext):
+            self._lines.append(line)
+        elif isinstance(line, DeferredLineBase):
             self._lines.append(line.with_prefix(self.prefix()))
         elif line.strip():
             self._lines.append(f"{self.prefix()}{line}")
@@ -440,12 +462,15 @@ class IndentedBuffer:
         if isinstance(other_code, IndentedBuffer):
             dedent = float("inf")
             for line in other_code._lines:
-                if line:
+                if not isinstance(line, LineContext) and line:
                     dedent = min(dedent, len(line) - len(line.lstrip()))
             if math.isinf(dedent):
                 dedent = 0
             for line in other_code._lines:
-                IndentedBuffer.writeline(self, line[dedent:])
+                if isinstance(line, LineContext):
+                    self._lines.append(line)
+                else:
+                    IndentedBuffer.writeline(self, line[dedent:])
         else:
             other_code = textwrap.dedent(other_code)
             if strip:
